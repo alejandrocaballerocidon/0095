788ab76c06cf12af92a4a89814f37ab1165e2213
Alex Caballero Cidon <alexcc_90@hotmail.com>
2014-03-25 20:35:57 +0100

    Añadida funcionalidad que hace que el chat termine siempre que el usuario ponga bye, ya sea en mayúsculas y minúsculas

--- SupportSystem.java
+++ SupportSystem.java
@@ -38,8 +38,8 @@ public class SupportSystem
 
         while(!finished) {
             String input = reader.getInput();
-            input = input.trim();
-            if(input.startsWith("bye")) {
+            
+            if(input.toLowerCase().trim().startsWith("bye")) {
                 finished = true;
             }
             else {
