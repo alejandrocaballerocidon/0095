c21732172da7d38a3df88c0685ece53c9ad2df2b
Alex Caballero Cidon <alexcc_90@hotmail.com>
2014-03-28 12:29:31 +0100

    Actividad 0092: modificadas las clases Responder y SupportSystem para que, imaginando que el usuario introduzca una sola palabra en el chat, la aplicación le conteste con una frase relacionada con dicha palabra.

--- Responder.java
+++ Responder.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -12,6 +12,7 @@ public class Responder
 {
     Random aleatorio;
     ArrayList<String> responses;
+    HashMap<String, String> respuestaPregunta;
     /**
      * Construct a Responder - nothing to do
      */
@@ -19,21 +20,34 @@ public class Responder
     {
         aleatorio = new Random();
         responses = new ArrayList();
+        respuestaPregunta = new HashMap<String, String>();
         
         responses.add("Response 1");
         responses.add("Response 2");
         responses.add("Response 3");
         responses.add("Response 4");
         responses.add("Response 5");
+        
+        respuestaPregunta.put("servicio", "Nuestro software ofrece diversos servicios.");
+        respuestaPregunta.put("plataformas", "Está diseñado para diversas plataformas.");
+        respuestaPregunta.put("fallos", "Estamos solucionando los problemas, en un rato el servicio funcionará con normalidad.");
+        respuestaPregunta.put("gratuito", "Actualmente disponemos de un software gratuito pero más limitado.");
+        
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String userInput)
     {
+        String response = null;
+        response = respuestaPregunta.get(userInput);
         
-        return responses.get(aleatorio.nextInt(responses.size()));
+        if (response == null){
+            response = responses.get(aleatorio.nextInt(responses.size()));
+        }
+        
+        return response;
     }
 }
